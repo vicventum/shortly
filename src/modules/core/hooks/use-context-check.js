dae74a2349760e833e02c6af8a68c621b00cf733
Víctor Álvarez <vicventum.dev@gmail.com>
2024-07-30 13:25:58 -0400

    Implementa context para URL

--- src/modules/core/hooks/use-context-check.js
+++ src/modules/core/hooks/use-context-check.js
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
 
-export function useCustomContext(contextToCheck, hookName) {
+export function useContextCheck(contextToCheck, hookName) {
   const context = useContext(contextToCheck)
   if (!context)
     throw new Error(`❌ ${hookName} must be used within a context provider`)
