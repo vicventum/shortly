6775f5a8a5245b4d26cb9b9f50558fa4c69889b7
Víctor Álvarez <vicventum.dev@gmail.com>
2024-08-01 22:04:42 -0400

    Corrige color de texto en banner en todos los temas

--- src/modules/brand/components/banner/BannerCta.jsx
+++ src/modules/brand/components/banner/BannerCta.jsx
@@ -3,7 +3,7 @@ import { BaseButton } from '@/modules/core/components/base/BaseButton'
 export function BannerCta({ ...prop }) {
   return (
     <div className='bg-boost-pattern-mobile md:bg-boost-pattern-desktop bg-secondary bg-cover py-24 text-center md:py-16'>
-      <h2 className='text-base-100'>Boost your links today</h2>
+      <h2 className='text-white'>Boost your links today</h2>
 
       <BaseButton className='mt-7 md:mt-9' variant='rounded' size='lg'>
         Get started
